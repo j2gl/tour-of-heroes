5e23ad44664b0304a353774a768aee3eb3c82f3a
Juan J. Garcia <juanjo.garcia@gmail.com>
2017-05-24 17:20:05 -0600

    Add dashboard on routing part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './herores.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
  ],
   declarations: [ 
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
