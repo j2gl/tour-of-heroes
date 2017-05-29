5e74fc699dfb6c09f9895a8b570bc115a1534e01
Juan J. Garcia <juanjo.garcia@gmail.com>
2017-05-29 16:28:33 -0600

    Add http calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule }    from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
-import { DashboardComponent }  from './dashboard.component';
-
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
@@ -23,9 +30,7 @@ import { DashboardComponent }  from './dashboard.component';
     HeroesComponent,
     HeroDetailComponent
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
